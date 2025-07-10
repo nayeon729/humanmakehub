cec224b422bb7d5dd4fd9c8b73eaefad6873d380
nayeon4729 <kimnayeon4729@gmail.com>
2025-07-10 14:29:27 +0900

    [김나연] 관리자 사용자 관리&전체 프로젝트 css 수정

--- frontend/src/components/AlertCard.js
+++ frontend/src/components/AlertCard.js
@@ -23,42 +23,43 @@ export default function AlertBox({
       sx={{
         backgroundColor: "#1976d2",
         color: "white",
-        p: 2,
+        p: 0.7,
         mt: 2,
         mb: 2,
         borderRadius: 2,
         position: "relative",
+        height: 60
       }}
     >
       {/* 닫기 버튼 */}
       <IconButton
         onClick={onClose}
-        sx={{ position: "absolute", top: 8, right: 8, color: "white" }}
+        sx={{ position: "absolute", top: -2, right: 2, color: "white" }}
       >
         <CloseIcon />
       </IconButton>
 
       <Stack direction="row" spacing={2} alignItems="flex-start">
-        <CheckCircleIcon sx={{ fontSize: 28, mt: 0.5 }} />
+        <CheckCircleIcon sx={{ fontSize: 26, mt:1}} />
 
         <Box flexGrow={1}>
-          <Typography variant="h6" fontWeight="bold">
+          <Typography variant="h8" fontWeight="bold">
             {title}
           </Typography>
-          <Typography variant="body2" sx={{ mt: 1 }}>
-            {description}
-          </Typography>
+          <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+            <Typography variant="body2" sx={{ mt: 1 }}>
+              {description}
+            </Typography>
 
-          {/* 버튼 영역 */}
-          <Stack direction="row" spacing={1} mt={2} justifyContent="flex-end">
-            <Button
-              variant="contained"
-              sx={{ backgroundColor: "white", color: "#005fb8" }}
-              onClick={onConfirm}
-            >
-              {confirmText}
-            </Button>
-          </Stack>
+            {/* 버튼 영역 */}
+              <Button
+                variant="contained"
+                sx={{ backgroundColor: "white", color: "#005fb8", position:'absolute', right:'12px', height:'25px',mt:1}}
+                onClick={onConfirm}
+              >
+                {confirmText}
+              </Button>
+          </Box>
         </Box>
       </Stack>
     </Paper>
