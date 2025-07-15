530a313b2db89b1da03778cc8715d793e3ee7779
juusingsing <ansjs1@naver.com>
2025-07-15 17:13:27 +0900

    [문주성] 주소추가

--- backend/main.py
+++ backend/main.py
@@ -31,6 +31,7 @@ def list_routes():
 # CORS 설정
 origins = [
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:3001",
     "http://124.58.88.165:3001",
     "http://192.168.0.97:3001",
